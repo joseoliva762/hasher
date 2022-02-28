9bc59c348e64cefeea87ec4224a68aad852ad30f
Joseoliva762 <joseolivacr@unimagdalena.edu.co>
2022-02-28 08:23:48 -0500

    [ADD] footer

---
+++ containers/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="grid place-content-center h-14 w-full md:max-w-md">
+                <p className="text-center text-hasher-gray w-full">Jose Oliva © 2022</p>
+                <p className="text-center text-hasher-gray w-full">Non Terrae Plus Ultra</p>
+            </footer>
+        </>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import AppContext from '@contexts/AppContext'
 import useInitialState from '@hooks/useInitialState'
 import Header from '@containers/Header';
+import Footer from '@containers/Footer';
 
 function MyApp({ 
   Component: App, 
@@ -16,6 +17,7 @@ function MyApp({
         <div className='bg-shop-white min-h-screen flex flex-col mx-auto items-center'>
           <Header />
           <App {...pageProps} />
+          <Footer />
         </div>
       </AppContext.Provider>    
     </>
