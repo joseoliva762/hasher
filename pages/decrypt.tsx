d00c5dd8efea050427f0ce048876bc2404b017be
Joseoliva762 <joseolivacr@unimagdalena.edu.co>
2022-02-28 00:33:18 -0500

    [FIX] div gap

--- pages/decrypt.tsx
+++ pages/decrypt.tsx
@@ -17,17 +17,17 @@ const Decrypt: NextPage = () => {
           <title>Hasher - Decrypt</title>
         </Head>
         <Container>
-            <div className="p-2 w-full">
-                <Subtitle subtitle='Decrypt' classname="px-1 w-full" />
-                <PasteSalt />
+            <div className="flex flex-col gap-1 items-center justify-start p-2 w-full">
+              <Subtitle subtitle='Decrypt' classname="px-1 w-full" />
+              <PasteSalt />
             </div>
             {!!state.salt && <>
-                <div className="p-2 w-full">
-                    <Subtitle subtitle='Type to decode' classname="px-1 w-full" />
-                    <ToDecode />
-                </div>
-                <Subtitle subtitle='Decoded' classname="px-2 w-full" />
-                <Result show="decrypted" />
+              <div className="flex flex-col gap-1 items-center justify-start p-2 w-full">
+                  <Subtitle subtitle='Type to decode' classname="px-1 w-full" />
+                  <ToDecode />
+              </div>
+              <Subtitle subtitle='Decoded' classname="px-2 w-full" />
+              <Result show="decrypted" />
             </>}
         </Container>  
       </>
